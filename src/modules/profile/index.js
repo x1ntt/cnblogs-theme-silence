3159647690097e7e33a02f6e27ab97d4b62f8a60
x1nt <2102084678@qq.com>
2022-09-22 22:55:37 +0800

    修复dark模式下进入网页avatar不变的问题

--- src/modules/profile/index.js
+++ src/modules/profile/index.js
@@ -3,9 +3,16 @@ import options from '@/consts/options';
 
 function buildProfile() {
     const avatar = options.avatar;
+    const avatar_dark = options.avatar_dark;
     const favicon = options.favicon;
-    if (avatar) {
+    const hour = new Date().getHours();
+    const modeKey = `silence-mode-${currentBlogApp}`;
+    const modeLoading = sessionStorage.getItem(modeKey) || (options.defaultMode == 'auto' ? (hour >= 6 && hour < 18 ? 'light' : 'dark') : options.defaultMode);
+    if (modeLoading == "light"){
         $('#sideBarMain').prepend(`<img class="esa-profile-avatar" src="${avatar}" />`);
+    }else{
+	if (avatar_dark)
+	    $('#sideBarMain').prepend(`<img class="esa-profile-avatar" src="${avatar_dark}" />`);
     };
     if (favicon) {
         $('#favicon').attr('href', favicon);
